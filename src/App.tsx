dca926b6def13afa23c3932fd25ffe83f5ce5454
Akofahi <39749693+Akofahi@users.noreply.github.com>
2022-12-12 01:31:50 +0300

    Revert "Deploy Github"
    This reverts commit d5681406bd2b23ed4f40080edada02bb00ba4973.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
         <Route path="/auth/login" element={<Login />} />
         <Route path="/auth/register" element={<Register />} />
         <Route path="/auth/reset-password" element={<ResetPassword />} />
-        <Route index path="/" element={<FilesList />} />
+        <Route index element={<FilesList />} />
         <Route path="*" element={<h1>Not found</h1>} />
       </Routes>
     </div>
